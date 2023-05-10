365bc5724465fcb9b441846fc65ae1f8776749cb
Fuaark <largeladphil@outlook.com>
2023-05-10 19:39:03 +0200

    first function that extracts demodata

--- prv.jl
+++ prv.jl
@@ -10,10 +10,24 @@ dfs = import_excel_files(dataPath)
 
 df = dfs[1]
 
-firstcolumn = names(df)[1]
-namecol = string.(df[:,firstcolumn])
+firstColumn = names(df)[1]
+nameCol = string.(df[:,firstColumn])
 
-namecol = strip.(namecol, r"\s+")
+nameCol = replace.(nameCol, r"\s+" => "")
 
 index = findfirst(x -> x == "TIME",namecol)
 
+demographicsData = df[1:index-1,:]
+
+
+function find_var(df,var)
+    for i in CartesianIndices(Matrix(df))
+        if typeof(df[i]) <: AbstractString && occursin(var, df[i])
+            return (i)
+        end
+    end
+end
+
+nameIdx = find_var(demographicsData,"Name")
+
+name = demographicsData[nameIdx]
